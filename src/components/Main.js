81322ff8b991617aed04e1527d6035c52b4e5961
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-09-26 20:14:59 +0200

    Made api call with side effect

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,64 @@
 /** @format */
 import React from 'react'
-import memesData from '../memeData';
+// import memesData from '../memeData';
 
 const Main = () => {
      
-     const [meme, setMeme] = React.useState()
+     const [meme, setMeme] = React.useState({
+          topText: '',
+          bottomText: '',
+          url: 'https://i.imgflip.com/1ur9b0.jpg'
+     });
+
+     const [allMemes, setAllMemes] = React.useState([])
      
      function getMemeImage() {
-          const memesArray = memesData.data.memes
-          const randomNumber = Math.floor(Math.random() * memesArray.length);
-          setMeme(memesArray[randomNumber].url)
+          const randomNumber = Math.floor(Math.random() * allMemes.length);
+          const imgUrl = allMemes[randomNumber].url;
+          setMeme(prevMeme => {
+               return {
+                    ...prevMeme,
+                    url: imgUrl
+               }
+          })
      }
+
+     function handleChange(e) {
+		setMeme((prevFormData) => {
+			const { name, value } = e.target;
+
+			return {
+				...prevFormData,
+				[name] : value
+			}
+		});
+     }
+     
+     React.useEffect(() => {
+          fetch('https://api.imgflip.com/get_memes')
+			.then((res) => res.json())
+               .then((data) => setAllMemes(data.data.memes));
+     }, [meme])
 
 	return (
 		<main>
 			<form>
 				<div className='form-control'>
-					<input type='text' placeholder='Top text' />
-					<input type='text' placeholder='Bottom text' />
+					<input onChange={handleChange} type='text' name="topText" value={meme.topText} />
+					<input onChange={handleChange} type='text' name="bottomText" value={meme.bottomText} />
 				</div>
 			</form>
 			<button onClick={getMemeImage}>Get a new meme image 🖼</button>
-               <div className='meme-wrapper'>
-				<img src={`${meme}`} alt="meme" />
+			<div className='meme-wrapper'>
+				<div className='top-bottom-text'>
+					<h1>{meme.topText}</h1>
+					<h1>{meme.bottomText}</h1>
+				</div>
+
+				<img src={`${meme.url}`} alt='meme' />
 			</div>
 		</main>
-     );
+	);
 };
 
 export default Main;
